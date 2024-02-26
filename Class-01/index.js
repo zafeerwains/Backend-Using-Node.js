e42bcda5d6d1a1183bf2dfac4695d542d881a4ab
Muhammad Zafeer <zafeerwains@gmail.com>
2024-02-26 07:23:36 +0500

    class-01

--- Class-01/index.js
+++ Class-01/index.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get("/Test",(req, res) => {
+    res.send("Heelo Testing done")
+})
 
 app.listen(process.env.PORT, () => {
     console.log(` app listening on PORT ${process.env.PORT}`)
